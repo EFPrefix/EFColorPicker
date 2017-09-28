15ba1700f9cc364f63aeef435d9bfe285d8bb51d
EyreFree <zheng.xu03@ele.me>
2017-09-28 12:39:29 +0800

    Setup

---
+++ Example/Pods/Target Support Files/EFColorPicker/EFColorPicker-dummy.m
@@ -0,0 +1,5 @@
+#import <Foundation/Foundation.h>
+@interface PodsDummy_EFColorPicker : NSObject
+@end
+@implementation PodsDummy_EFColorPicker
+@end
---
+++ Example/Pods/Target Support Files/Pods-EFColorPicker_Tests/Pods-EFColorPicker_Tests-dummy.m
@@ -0,0 +1,5 @@
+#import <Foundation/Foundation.h>
+@interface PodsDummy_Pods_EFColorPicker_Tests : NSObject
+@end
+@implementation PodsDummy_Pods_EFColorPicker_Tests
+@end
