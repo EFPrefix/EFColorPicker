6aa09498d2152d29daee527c2f3dc7c036b219e6
EyreFree <zheng.xu03@ele.me>
2017-10-16 21:05:26 +0800

    Update demo

--- Example/EFColorPicker/AppDelegate.swift
+++ Example/EFColorPicker/AppDelegate.swift
@@ -33,6 +33,7 @@ class AppDelegate: UIResponder, UIApplicationDelegate {
 
     func application(_ application: UIApplication, didFinishLaunchingWithOptions launchOptions: [UIApplicationLaunchOptionsKey: Any]?) -> Bool {
 
+        UIApplication.shared.isStatusBarHidden = false
         return true
     }
 
