6acbf66c06be8430209173c33e581ec8905a017e
EyreFree <eyrefree@eyrefree.org>
2021-07-18 23:49:53 +0800

    chore: add Startup.sh

--- Example/Pods/Target Support Files/EFColorPicker/EFColorPicker-dummy.m
+++
@@ -1,5 +0,0 @@
-#import <Foundation/Foundation.h>
-@interface PodsDummy_EFColorPicker : NSObject
-@end
-@implementation PodsDummy_EFColorPicker
-@end
--- Example/Pods/Target Support Files/Pods-EFColorPicker_Example/Pods-EFColorPicker_Example-dummy.m
+++
@@ -1,5 +0,0 @@
-#import <Foundation/Foundation.h>
-@interface PodsDummy_Pods_EFColorPicker_Example : NSObject
-@end
-@implementation PodsDummy_Pods_EFColorPicker_Example
-@end
